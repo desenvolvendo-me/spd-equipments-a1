1db6c06762c8a4ba2f0101c693684fc54442de35
Marco Castro <marcodotcastro@gmail.com>
2022-11-16 08:52:37 -0300

    build: create project

---
+++ lib/meu_estudo_ruby.rb
@@ -0,0 +1,6 @@
+require "meu_estudo_ruby/version"
+
+module MeuEstudoRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/meu_estudo_ruby/version.rb
@@ -0,0 +1,3 @@
+module MeuEstudoRuby
+  VERSION = "0.1.0"
+end
